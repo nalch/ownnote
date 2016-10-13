0874fa30c1c978f0edba912f64832766f8577805
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-10-13 14:54:54 +0200

    Add global Ownnote Javascript

---
+++ js/ownnote.js
@@ -0,0 +1,10 @@
+/* global OwnNote */
+(function ($, OC, t) {
+	"use strict";
+	var Ownnote = {
+		config: {},
+		/** Map of the whole Ownnote, built as we navigate through folders */
+		appName: 'ownnote',
+	}
+	window.Ownnote = Ownnote;
+})(jQuery, OC, t);
